6483feca70140e7b7213da6e2c0d913a3afd554c
Startail the 'Coon <startail@outlook.com>
2024-04-14 19:04:30 +0200

    psr-4 compliance

--- src/Models/Chain/ContractVoteClaim.php
+++ src/Models/Chain/ContractVoteClaim.php
@@ -4,7 +4,7 @@ namespace CoonDesign\phpGridcoin\Models\Chain;
 
 class ContractVoteClaim {
     var int $version;
-    var ContractVoteClaimMagntiude $magnitude_claim;
+    var ContractVoteClaimMagnitude $magnitude_claim;
     /** @var ContractVoteClaimBalance[] $balance_claim */
     var array $balance_claim;
 }
--- src/Models/Chain/ContractVoteClaimMagnitude.php
+++ src/Models/Chain/ContractVoteClaimMagnitude.php
@@ -3,7 +3,7 @@
 namespace CoonDesign\phpGridcoin\Models\Chain;
 
 
-class ContractVoteClaimMagntiude {
+class ContractVoteClaimMagnitude {
     var string $mining_id;
     var string $beacon_txid;
     var string $signature;
